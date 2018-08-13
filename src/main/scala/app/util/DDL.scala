b1ac25cc204311e2ef7349ebb1b6139dd6bbcf7c
Yasuhiro Suzuki <ysuzuki1603@gmail.com>
2018-08-13 15:43:36 +0900

    Fix DDL

--- src/main/scala/app/util/DDL.scala
+++ src/main/scala/app/util/DDL.scala
@@ -5,19 +5,22 @@ object DDL {
   val createArticlesTable =
     """CREATE TABLE IF NOT EXISTS `articles` (
          `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-         `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+         `published` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
          `title` TEXT NOT NULL,
          `content` TEXT NOT NULL,
          `thumbnail` VARCHAR(255) NOT NULL,
          `link` VARCHAR(255) NOT NULL,
          `organization_id` BIGINT(20) NOT NULL,
          `del_flg` tinyint(4) NOT NULL DEFAULT '0',
-         `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-         `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+         `create_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+         `update_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
          PRIMARY KEY (`id`),
          UNIQUE INDEX `id_UNIQUE` (`id` ASC),
          INDEX `organization_id_idx` (`organization_id` ASC))
-       ENGINE = InnoDB"""
+       ENGINE = InnoDB
+       DEFAULT CHARACTER SET = utf8mb4
+       COLLATE = utf8mb4_unicode_ci;
+    """
 
   val createCategoriesTable =
     """CREATE TABLE IF NOT EXISTS `categories` (
@@ -30,6 +33,9 @@ object DDL {
          PRIMARY KEY (`id`),
          UNIQUE INDEX `id_UNIQUE` (`id` ASC),
          INDEX `article_id_idx` (`article_id` ASC))
-       ENGINE = InnoDB"""
+       ENGINE = InnoDB
+       DEFAULT CHARACTER SET = utf8mb4
+       COLLATE = utf8mb4_unicode_ci;
+    """
 
 }
